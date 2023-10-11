c80e48275170d74560bec4a4089b2c5ae7d2b29e
Ericson J. Santos <ericson.takay@gmail.com>
2023-10-11 09:51:14 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,33 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+	private static List<Filme> filmes = new List<Filme>();
+	private static int id = 0;
+
+	[HttpPost]
+	public void AdicionaFilme([FromBody]Filme filme) 
+	{
+		filme.Id = id++;
+		filmes.Add(filme);
+		Console.WriteLine(filme.Titulo);
+		Console.WriteLine(filme.Duracao);
+	}
+
+	[HttpGet]
+	public IEnumerable<Filme> RecuperarFilmes(int skip, int take) 
+	{
+		return filmes.Skip(skip).Take(take);
+	}
+
+	[HttpGet("{id}")]
+	public Filme? RecuperarFilmePorId(int id)
+	{
+		return filmes.FirstOrDefault(filme => filme.Id == id);
+	}
+}
